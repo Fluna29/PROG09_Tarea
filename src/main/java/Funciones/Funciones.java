df6f1e93701f514539a9a12e8d2a95088740fe89
Fluna29 <fluna293107@gmail.com>
2024-04-21 21:01:04 +0200

    Tarea finalizada

--- src/main/java/Funciones/Funciones.java
+++ src/main/java/Funciones/Funciones.java
@@ -3,7 +3,8 @@ package Funciones;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * En esta clase van a ir todas las funciones a utilizar en nuestra clase PROG09_Tarea
@@ -11,6 +12,7 @@ import java.nio.file.attribute.BasicFileAttributes;
  */
 public class Funciones {
     
+    private static final Scanner scanner = new Scanner(System.in);
     private static final String KEY = "user.home";
     private static final String DIRECTORY_PATH = System.getProperty(KEY);
 
@@ -22,8 +24,6 @@ public class Funciones {
      * @throws IOException
      */
     public static void mostrarDirectoriosUsuario () throws IOException{
-
-        //TODO Escribir fecha ultima modificacion + tamaño en KiloBytes
 
         Path directory = Paths.get(DIRECTORY_PATH);
 
@@ -41,7 +41,7 @@ public class Funciones {
             }
 
             System.out.println(listaDirectorios.toString());
-
+        
         }finally{
             if (items != null) {
                 items.close();
@@ -49,20 +49,115 @@ public class Funciones {
         }
     }
 
+    /**
+     * Cuando se seleccione esta opción se pedirá al usuario el nombre de un nuevo archivo de texto que se creará en la carpeta de de usuario. 
+     * Si no existe un archivo con ese nombre lo creará con una única línea de texto con el nombre y apellidos del alumno. 
+     * Si ya existe, simplemente mostrará un mensaje indicando que ya existe un archivo con ese nombre y no hará nada más.
+     * @param nombreArchivo
+     * @throws IOException
+     */
     public static void crearArchivoUsuario (String nombreArchivo) throws IOException{
 
-        Path directory = Paths.get(DIRECTORY_PATH);
+        nombreArchivo += ".txt";
 
-        DirectoryStream<Path> items = null;
-        try{
-            Files.newDirectoryStream(directory);
-            for(Path file : items){
-                if (true) {
-                    
-                }
+        Path nuevoArchivo = Paths.get(DIRECTORY_PATH, nombreArchivo);
+        if (!Files.exists(nuevoArchivo)) {
+            try{
+                Files.createFile(nuevoArchivo);
+                Files.write(nuevoArchivo, "Francisco Luna Raya".getBytes());
+                System.out.println("\nArchivo creado correctamente");
+            }catch(IOException e){
+                System.out.println("\nError al escribir en el archivo" + e.getMessage());
             }
-        }finally{
-
+        } else{
+            System.out.println("\nNo se ha podido crear el archivo, porque ya existe un archivo con ese nombre");
         }
+    }
+
+    /**
+     * Cuando se seleccione esta opción se pedirá al usuario el nombre de un archivo de texto que debe estar en la carpeta de de usuario. 
+     * Si dicho archivo existe se mostrará el contenido del archivo por la consola mostrando cada línea del archivo en una línea diferente en la consola. 
+     * Además, al final de todas las líneas se mostrará una línea adicional indicando cuantas lineas de texto tenía el fichero. 
+     * Si no existe el archivo con el nombre indicado, simplemente se mostrará un mensaje indicándolo al usuario.
+     * @param nombreArchivo
+     */
+    public static void mostrarContenidoArchivo (String nombreArchivo){
+
+        Path archivo = Paths.get(DIRECTORY_PATH, nombreArchivo);
+
+        if (Files.exists(archivo) && Files.isRegularFile(archivo)){
+            try {
+
+                List<String> lineas = Files.readAllLines(archivo);
+                System.out.println("\nContenido del archivo: ");
+
+                for (String linea : lineas){
+                    System.out.println(linea);
+                }
+
+                System.out.println("\nNúmero de líneas del archivo: " + lineas.size());
+            } catch (IOException e) {
+                System.out.println("\nError al leer el archivo: " + e.getMessage());
+            }
+        }else{
+            System.out.println("\nEl archivo no existe");
+        }
+    }
+
+    /**
+     * Cuando se seleccione esta opción se pedirá al usuario el nombre de un archivo de texto que debe estar en la carpeta de de usuario. 
+     * Si dicho archivo existe se pedirá al usuario que introduzca una nueva línea de texto por la consola y dicha línea se añadirá al final del archivo. 
+     * Si no existe el archivo con el nombre indicado, simplemente se mostrará un mensaje indicándolo al usuario.
+     * @param nombreArchivo
+     */
+    public static void anyadirLinea (String nombreArchivo){
+
+        
+        Path archivo = Paths.get(DIRECTORY_PATH, nombreArchivo);
+
+        if(Files.exists(archivo) && Files.isRegularFile(archivo)){
+            try {
+                System.out.println("\nIntroduce la línea de texto a añadir en el archivo de texto: ");
+                String texto = scanner.nextLine();
+
+                Files.write(archivo, (texto + System.lineSeparator()).getBytes(), java.nio.file.StandardOpenOption.APPEND);
+                System.out.println("\nSe ha escrito en el archivo correctamente");
+            } catch (IOException e) {
+                System.out.println("\nError al escribir en el archivo: " + e.getMessage());
+            }
+        }else{
+            System.out.println("\nEl archivo no existe");
+        }
+    }
+
+    /**
+     * Cuando se seleccione esta opción se pedirá al usuario el nombre de un archivo de texto que debe estar en la carpeta de de usuario. 
+     * Si dicho archivo existe se borrará y se mostrará un mensaje de confirmación al usuario por la consola. 
+     * Si no existe el archivo con el nombre indicado, simplemente se mostrará un mensaje indicándolo al usuario.
+     * @param nombreArchivo
+     */
+    public static void eliminarArchivo(String nombreArchivo) {
+
+        Path archivo = Paths.get(DIRECTORY_PATH, nombreArchivo);
+
+        if (Files.exists(archivo) && Files.isRegularFile(archivo)) {
+
+            System.out.println("\n¿Desea borrar el archivo con nombre '" + nombreArchivo + "' (s/n)?");
+            String respuetaBorrado = scanner.nextLine();
+            scanner.nextLine();
+
+            if (respuetaBorrado.equalsIgnoreCase("s")) {
+                    try {
+                        Files.delete(archivo);
+                        System.out.println("\nEl archivo con nombre: '" + nombreArchivo + "' ha sido eliminado correctamente.");
+                    } catch (IOException e) {
+                        System.out.println("\nError al eliminar el archivo: " + e.getMessage());
+                    }
+            }else{
+                System.out.println("\nHa decidido no borrar el archivo");
+            }
+        } else {
+            System.out.println("\nEl archivo con nombre'" + nombreArchivo + "' no existe.");
+        }     
     }
 }
--- src/main/java/Principal/PROG09_Tarea.java
+++ src/main/java/Principal/PROG09_Tarea.java
@@ -1,18 +1,155 @@
 package Principal;
 import Funciones.Funciones;
+
+import java.io.IOException;
+import java.util.Scanner;
 /**
- *
- * @author fluna
+ * En esta clase vamos a ejecutar la clase main, importando la clase Funciones que es donde
+ * tenemos todas las funciones de nuestra tarea.
+ * @author Francisco Luna Raya
  */
 public class PROG09_Tarea {
 
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+    
+        int eleccion;
+
+        do{
+            menu();
+            System.out.println("\nIntroduzca un número acorde al menú (1-6)");
+            eleccion = scanner.nextInt();
+            if (0< eleccion && eleccion < 7){
+                switch (eleccion) {
+                    case 1:
+                        mostrarDirectoriosUsuario();
+                        scanner.nextLine();
+                        break;
+                    case 2:
+                        nuevoArchivo();
+                        scanner.nextLine();
+                        break;
+                    case 3:
+                        mostrarContenidoArchivo();
+                        scanner.nextLine();
+                        break;
+                    case 4:
+                        anyadirLinea();
+                        scanner.nextLine();
+                        break;
+                    case 5:
+                        eliminarArchivo();
+                        scanner.nextLine();
+                        break;
+                    case 6:
+                        System.out.println("\nHasta la próxima");
+                        break;
+                    default:
+                        System.out.println("\nERROR");
+                        break;
+                }  
+            }
+        }while (eleccion != 6);
+    }
+
+    /**
+     * Esta función devuelve el texto del menú visual
+     */
+    private static void menu(){
+        System.out.println("\n====MENÚ PRINCIPAL====");
+        System.out.println(" 1. Mostrar directorios de la carpeta de usuario");
+        System.out.println(" 2. Crear un nuevo archivo de texto en la carpeta de usuario");
+        System.out.println(" 3. Mostrar el contenido de un archivo de texto de la carpeta de usuario");
+        System.out.println(" 4. Añadir una nueva línea de texto a un archivo de la carpeta de usuario");
+        System.out.println(" 5. Eliminar un archivo de la carpeta de usuario");
+        System.out.println(" 6. Salir");
+    }
+
+
+    /**
+     * En esta función llamamos a la lógica de la función en la clase Funciones,
+     * pero lo único que hacemos aquí es controlar el error.
+     */
+    private static void mostrarDirectoriosUsuario (){
         try {
-            
             Funciones.mostrarDirectoriosUsuario();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("\nNo se pueden mostrar los directorios " + e.getMessage());
         }
+    }
+
+    /**
+     * En esta función llamamos a la lógica de la función en la clase Funciones,
+     * aquí se pide al usuario que introduzca el nombre del archivo que quiera crear
+     * y mediante el uso de un try-catch se contrla la excepción.
+     */
+    private static void nuevoArchivo (){
         
+        System.out.println("\nIntroduzca el nombre del archivo a crear: ");
+        String nombreArchivo = scanner.nextLine();
+        while (nombreArchivo.isEmpty()) {
+            nombreArchivo = scanner.nextLine().trim(); // trim() para eliminar espacios en blanco
+            if (nombreArchivo.isEmpty()) {
+                System.out.println("Por favor, ingrese un valor válido.");
+            }
+        }
+        try {
+            Funciones.crearArchivoUsuario(nombreArchivo);
+        } catch (IOException e) {
+            System.out.println("\nError al escribir en el archivo" + e.getMessage());
+        }
+    }
+
+    /**
+     * En esta función llamamos a la lógica de la función en la clase Funciones,
+     * aquí se pide al usuario que introduzca el nombre del archivo del que quiera ver
+     * el contenido.
+     */
+    private static void mostrarContenidoArchivo (){
+
+        System.out.println("\nIntroduzca el nombre del archivo para mostrar su contenido: ");
+        String nombreArchivo = scanner.nextLine();
+        while (nombreArchivo.isEmpty()) {
+            nombreArchivo = scanner.nextLine().trim(); // trim() para eliminar espacios en blanco
+            if (nombreArchivo.isEmpty()) {
+                System.out.println("Por favor, ingrese un valor válido.");
+            }
+        }
+        Funciones.mostrarContenidoArchivo(nombreArchivo);
+    }
+
+    /**
+     * En esta función llamamos a la lógica de la función en la clase Funciones,
+     * aquí se pide al usuario que introduzca el nombre del archivo en el que quiera añadir una línea.
+     * Tras esto, la propia función de la clase Funciones, le pide que introduzca el texto de la línea.
+     */
+    private static void anyadirLinea (){
+        
+        System.out.println("\nIntroduzca el nombre del archivo para añadir una línea: ");
+        String nombreArchivo = scanner.nextLine();
+        while (nombreArchivo.isEmpty()) {
+            nombreArchivo = scanner.nextLine().trim(); // trim() para eliminar espacios en blanco
+            if (nombreArchivo.isEmpty()) {
+                System.out.println("Por favor, ingrese un valor válido.");
+            }
+        }
+        Funciones.anyadirLinea(nombreArchivo);
+    }
+
+    /**
+     * En esta función llamamos a la lógica de la función en la clase Funciones,
+     * aquí se pide al usuario que introduzca el nombre del archivo a eliminar.
+     * Tras esto, la propia función de la clase Funciones, le pide que una confirmación antes de eliminarlo.
+     */
+    private static void eliminarArchivo(){
+        System.out.println("\nIntroduzca el nombre del archivo a eliminar: ");
+        String nombreArchivo = scanner.nextLine();
+        while (nombreArchivo.isEmpty()) {
+            nombreArchivo = scanner.nextLine().trim(); // trim() para eliminar espacios en blanco
+            if (nombreArchivo.isEmpty()) {
+                System.out.println("Por favor, ingrese un valor válido.");
+            }
+        }
+        Funciones.eliminarArchivo(nombreArchivo);
     }
 }
